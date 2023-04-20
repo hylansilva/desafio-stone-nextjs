3fcdbd80189bb564717b75ef4ce00ff7eb6f41ee
Hylan Silva <hylan.00000847839@unicap.br>
2023-04-19 21:30:37 -0300

    🚧 Em progresso... services/api.ts

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,9 @@
 
-import './headerStyle.css'
+import './headerStyle.css';
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
 
-import Image from 'next/image'
-
-import logo from '../../../public/logo.png'
-
-export default function Header(){
-
+export default async function Header(){
   return(
     <header>
       <div className="logo">
@@ -16,7 +13,7 @@ export default function Header(){
       />
     </div>
     <div className="text">
-        <h3>   |    UTC</h3>
+        <h3>  |  UTC</h3>
       <p>Dados de câmbio disponibilizados pela Morningstar.</p>
     </div>
     </header>
---
+++ src/services/api.ts
@@ -0,0 +1,4 @@
+export const returnapi = (dol:number, dolv:number, iof:number) =>{
+  const moneyBuild =((dol+iof)+(dolv+iof))
+  return
+}
